6a2d0e9e627408fc722901d832144bb18510153f
Chris Joakim <christopher.joakim@gmail.com>
2015-07-31 15:28:17 -0400

    tests pass again after fixing test-src/spec_helper.coffee for latest jasmine version/syntax.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
           join: true
         },
         files: {
-          'lib/m26.js':                      ['src/*.coffee'],
+          'lib/m26.js':                      ['src/m26*.coffee'],
 
           'test/m26_age_spec.js':            ['test-src/m26_age_spec.coffee'],
           'test/m26_age_calculator_spec.js': ['test-src/m26_age_calculator_spec.coffee'],
@@ -16,6 +16,7 @@ module.exports = function (grunt) {
           'test/m26_distance_spec.js':       ['test-src/m26_distance_spec.coffee'],
           'test/m26_elapsed_time_spec.js':   ['test-src/m26_elapsed_time_spec.coffee'],
           'test/m26_speed_spec.js':          ['test-src/m26_speed_spec.coffee'],
+          'test/spec_helper.js':             ['test-src/spec_helper.coffee'],
 
           'examples.js':                     ['test-src/examples.coffee']
         }
